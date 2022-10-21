74426a153839d4c32ec7f833e5dd710a7995dff6
tonga <grojas89@hotmail.com>
2022-10-21 20:24:40 -0300

    agregamos header y footer moobile archivos html y sus css

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path')
 
 const app = express()
 
+app.use(express.static('public'))
+
+
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname + '/views/index.html'))
 })
