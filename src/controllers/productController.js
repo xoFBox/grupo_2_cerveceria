f42aafc7e805762b95e12210ad4879e59ac6116a
nahir-karin <nahirkabraham@gmail.com>
2022-11-07 23:51:55 -0300

    update carrito

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,11 @@
 const productController = {
     detail(req, res){
         res.render('productDetail', {style: 'css/productDetail.css'});
-    }
+    },
+
+    cart(req, res){
+        res.render('productCart', {style: '/css/productCart.css'});
+    },
 }
 
 module.exports = productController;
